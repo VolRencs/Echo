d3b101a1d4551b2cf1aafd43411e5f0e37d2a213
Fodicat <angel.v.lastah@gmail.com>
2025-09-18 00:50:39 +0300

    Update

--- Assets/Scripts/Map/Movement.gd
+++ Assets/Scripts/Map/Movement.gd
@@ -1,8 +1,32 @@
 extends CharacterBody3D
 
+const MOUSE_SENSITIVITY = 0.004
+const SMOOTH_SPEED = 10
+
+var smooth_x = 0.0
+var smooth_y = 0.0
+
 @export var speed: float = 5.0
 @export var jump_velocity: float = -5.0  # Отрицательное значение для прыжка вверх
 @export var gravity: float = 9.8
+
+@onready var head: = $"."
+@onready var camera: = $CameraPoint/Camera3D
+
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED and event is InputEventMouseMotion:
+		smooth_x = lerp(smooth_x, event.relative.x, SMOOTH_SPEED * get_process_delta_time())
+		smooth_y = lerp(smooth_y, event.relative.y, SMOOTH_SPEED * get_process_delta_time())
+
+		head.rotate_y(-smooth_x * MOUSE_SENSITIVITY)
+		camera.rotate_x(clamp(-smooth_y * MOUSE_SENSITIVITY + camera.rotation.x, deg_to_rad(-80), deg_to_rad(80)) - camera.rotation.x)
+	
+	camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
 
 func _physics_process(delta):
 	# Применяем гравитацию
