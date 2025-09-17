1f5a09eebcbed61adfdd5c9ec277a42180cbfebf
VolRenCS <vladdragunov@protonmail.com>
2025-09-17 03:28:44 +0300

    Clear

--- Assets/Scripts/MainMenu/Animationcontriler.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-@export var main_menu_panel: Node
-
-# Вызывается, например, из AnimationPlayer -> Call Method Track
-func show_main_menu() -> void:
-	if main_menu_panel:
-		main_menu_panel.visible = true
