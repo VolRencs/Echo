1129af5d0b05970014f5e58ba2685751f330d9f6
VolRenCS <vladdragunov@protonmail.com>
2025-11-07 21:27:56 +0300

    Update

--- Scripts/Player/Lighter.gd
+++ Scripts/Player/Lighter.gd
@@ -4,36 +4,31 @@ extends SpotLight3D
 @export var energy_off: float = 0.0
 @export var fade_speed: float = 8.0
 @export var flicker_enabled: bool = false
-@export var flicker_intensity: float = 0.5
-@export var flicker_frequency: float = 10.0
+@export var flicker_intensity: float = 0.3
+@export var flicker_frequency: float = 12.0
 @export var offset: Vector3 = Vector3(0.2, -0.3, -0.5)
 
 var is_on: bool = false
-var target_energy: float = 0.0
 var flicker_timer: float = 0.0
+var _sound: AudioStreamPlayer = null
 
 func _ready():
 	light_energy = energy_off
-	target_energy = energy_off
-	transform.origin = offset
+	global_transform.origin = get_parent().global_transform.origin + offset
+	
+	for node in get_tree().get_nodes_in_group("Sound"):
+		if node.name == "Lighter" and node is AudioStreamPlayer:
+			_sound = node
+			break
 
 func _process(delta):
 	if Input.is_action_just_pressed("flashlight"):
-		_toggle_flashlight()
-
-	if !is_equal_approx(light_energy, target_energy):
-		light_energy = lerp(light_energy, target_energy, fade_speed * delta)
+		is_on = !is_on
+		if _sound: _sound.play()
+	
+	var target = energy_on if is_on else energy_off
+	light_energy = lerp(light_energy, target, fade_speed * delta)
 
 	if flicker_enabled and is_on:
 		flicker_timer += delta * flicker_frequency
-		var flicker = sin(flicker_timer) * flicker_intensity
-		light_energy = clamp(target_energy + flicker, energy_off, energy_on)
-
-func _toggle_flashlight():
-	is_on = !is_on
-	target_energy = energy_on if is_on else energy_off
-
-	for player in get_tree().get_nodes_in_group("Sound"):
-		if player.name == "Lighter" and player is AudioStreamPlayer:
-			player.play()
-			break
+		light_energy = clamp(light_energy + sin(flicker_timer) * flicker_intensity * 0.2, energy_off, energy_on)
